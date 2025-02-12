0fb4524b68d47b9014f8e64d3262e0d32225440a
Anshul Rawal <104826906+anshul2071@users.noreply.github.com>
2025-02-12 12:21:06 +0545

    commit

--- switch.js
+++ switch.js
@@ -1,7 +1,10 @@
 
   // -----Switch Operation---- //
+// A switch statement is used to perform different actions based on different conditions. It compares a given value against multiple possible cases and executes the matching block of code.
+ 
+//example
 
- const num1 = Number(prompt("Enter the number: "));
+const num1 = Number(prompt("Enter the number: "));
  const operators = prompt("Enter Operation to perform (+,*,/,-):");
  const num2 = Number(prompt("Enter the number: "));
  
---
+++ whileloop.js
@@ -0,0 +1,11 @@
+//--------While Loop-----------//
+
+//checks the condition and then runs the loop.
+
+//print even no from 0 to 10
+let i = 0;
+
+while(i<=10){
+    console.log(`value = ${i}`);
+    i +=2; 
+}
