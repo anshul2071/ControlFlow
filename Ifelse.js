cae123352c2db36e858be8d941c370ed6ec05f4e
Anshul Rawal <104826906+anshul2071@users.noreply.github.com>
2025-02-12 08:01:55 +0545

    first commit

---
+++ Ifelse.js
@@ -0,0 +1,51 @@
+
+
+
+                                 
+
+
+
+ // --------IF ELSE CASE--------//
+ 
+ let score = prompt("Enter the score");
+ if(score!= Number && score >100){
+    alert("Invalid input");
+ }
+ 
+else {
+  if (score >= 90 && score <= 99) {
+     console.log("Grade A+");
+} 
+ else if (score >= 80 && score <= 89) {
+     console.log("Grade A");
+ } 
+ else if (score >= 70 && score <= 79) {
+     console.log("Grade B+");
+ }
+  else {
+     console.log("Fail");
+ }
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ switch.js
@@ -0,0 +1,41 @@
+
+  // -----Switch Operation---- //
+
+ const num1 = Number(prompt("Enter the number: "));
+ const operators = prompt("Enter Operation to perform (+,*,/,-):");
+ const num2 = Number(prompt("Enter the number: "));
+ 
+ let result;
+ 
+ 
+ switch(operators){
+     case "+":
+         result = num1 + num2;
+         break;
+ 
+     case "-":
+         result = num1-num2;
+         break;
+ 
+     case "*":
+         result = num1*num2;
+         break;
+ 
+     case "/":
+         if (num2 !== 0){
+             result = num1/num2;
+         }
+         else {
+             result = "Error: Division by zero is not allowed";
+             alert("not allowed")
+         }
+ 
+         break;
+ 
+     default:
+         result = alert("Invalid opertators");
+     
+ }
+ 
+ console.log(`Result : ${result}`)
+ 
